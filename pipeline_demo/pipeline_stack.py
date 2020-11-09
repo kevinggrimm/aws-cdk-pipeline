4541d48b2ca08f36072fd59dc7851a8eb52bf584
Kevin Grimm <kevin.g.grimm1@gmail.com>
2020-11-09 16:43:24 -0700

    revert to install command

--- pipeline_demo/pipeline_stack.py
+++ pipeline_demo/pipeline_stack.py
@@ -33,9 +33,9 @@ class PipelineStack(core.Stack):
           synth_action=pipelines.SimpleSynthAction(
             source_artifact=source_artifact,
             cloud_assembly_artifact=cloud_assembly_artifact,
-            install_commands=['npm install -g aws-cdk && pip install -r requirements.txt'],
+            install_command='npm install -g aws-cdk && pip install -r requirements.txt',
             synth_command='cdk synth')
-          )
+        )
 
         # Can be a different account and region
         # As long as you have permissions it will work
